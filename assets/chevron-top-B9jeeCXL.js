54d7a80d2de943df6cfcfd5373ef3605a90b1c3a
Jess9400 <Jess9400@users.noreply.github.com>
2026-03-24 13:18:32 +0000

    deploy: 3317176916b41be04e6c7767bb8291c23f5f7031

---
+++ assets/chevron-top-B9jeeCXL.js
@@ -0,0 +1,8 @@
+import{w as o}from"./core-pblTofq8.js";import"./index-BXXTZxB0.js";import"./events-DOT34ogD.js";import"./index.es-oS31c5Vd.js";import"./index-nibyPLVP.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M14.54 11.04a1 1 0 0 1-1.41 0L8 5.92l-5.13 5.12a1 1 0 1 1-1.41-1.41l5.83-5.84a1 1 0 0 1 1.42 0l5.83 5.84a1 1 0 0 1 0 1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{t as chevronTopSvg};
