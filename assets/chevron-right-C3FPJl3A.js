02e30cd5a457a6148fd62fd5846c8915b84ad8d2
Jess9400 <Jess9400@users.noreply.github.com>
2026-03-24 16:49:15 +0000

    deploy: 397df39625013ae216b7db5d0cfc1002c37c7044

--- assets/chevron-right-C3FPJl3A.js
+++
@@ -1,8 +0,0 @@
-import{w as o}from"./core-DtofdH5p.js";import"./index-CC2Q7ZOb.js";import"./events-DLH5I551.js";import"./index.es-D7_Ihvvu.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{p as chevronRightSvg};
