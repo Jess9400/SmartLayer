93a469b5c52fe2786a77b2b673ac4f77d79a0a26
Jess9400 <Jess9400@users.noreply.github.com>
2026-03-24 09:05:21 +0000

    deploy: d6430a1a7db4f76374daa7d5146b39e6f81e7aaa

---
+++ assets/cursor-BzIdiXQE.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-CQC92C8e.js";import"./index-yi5auEDO.js";import"./events-DUnOQsJZ.js";import"./index.es-BDV535j-.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
