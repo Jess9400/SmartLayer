6f020610aa5b0a262417c9b78ae2e6a4c9fdd350
Jess9400 <Jess9400@users.noreply.github.com>
2026-03-25 10:33:04 +0000

    deploy: 9deafa2106169319a8a5849cd755be7e73f12cb5

--- assets/cursor-CjMS5xVa.js
+++
@@ -1,3 +0,0 @@
-import{w as o}from"./core-UFWV81ar.js";import"./index-R3nT0I7w.js";import"./events-BlitVyRb.js";import"./index.es-C6_zRcL8.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
