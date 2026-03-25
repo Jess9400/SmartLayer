69091d68f59910feef98a0582f6bb1609d658e05
Jess9400 <Jess9400@users.noreply.github.com>
2026-03-25 10:22:57 +0000

    deploy: f1448208527662e43d3855f63ccf49ddf5962d94

--- assets/all-wallets-BhlOuIzg.js
+++
@@ -1,6 +0,0 @@
-import{w as o}from"./core-BH5pf31J.js";import"./index-DvpMEPwj.js";import"./events-42s9R2pZ.js";import"./index.es-jsSixjEU.js";import"./index-nibyPLVP.js";const p=o`<svg fill="none" viewBox="0 0 24 24">
-  <path
-    style="fill: var(--wui-color-accent-100);"
-    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
-  />
-</svg>`;export{p as allWalletsSvg};
